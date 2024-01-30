12fcfa4243eb0b7d8ac6dfa9516e512ef72ad946
Ghazia Maqbool <ghaziamaqbool89@gmail.com>
2024-01-30 16:15:47 +0500

    string conversions

--- 01_basics/03_converaionOperation.js
+++ 01_basics/03_converaionOperation.js
@@ -60,3 +60,44 @@ let someNumber = 61
 let stringNumber=String(someNumber)
 console.log(stringNumber); //61
 console.log(typeof stringNumber); //string
+
+console.log("_________________________Operations__________________________")
+
+let value = 3
+let negValue = -value
+// console.log(negValue); //-3
+
+// console.log(2+2);//4
+// console.log(2-2);//0
+// console.log(2*2);//4
+// console.log(2**3);//8
+// console.log(2/3);//0.6666666666666666
+// console.log(2%3);//2
+
+let str1 = "hello"
+let str2 = " ghazia"
+
+let str3 = str1 + str2
+console.log(str3); //hello ghazia
+
+console.log("1" + 2); //12
+console.log(1 + "2"); //"12"
+console.log("1" + "2"); //"12"
+console.log("1" + 2 + 2); //"122"
+console.log(1 + 2 + "2"); //32
+
+console.log(+true); //1
+console.log(true); //true
+console.log(+""); //0
+
+let num1, num2, num3
+
+num1 = num2 = num3 =2 + 2 
+
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter); //101
+
+let gameCounter1 =100
+++gameCounter1;
+console.log(gameCounter1); //101
