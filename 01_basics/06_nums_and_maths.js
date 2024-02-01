f6c9c5aa1d9c731d039a1d17b9c662af369ae465
Ghazia Maqbool <ghaziamaqbool89@gmail.com>
2024-02-01 17:38:55 +0500

    basics of  js

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -10,6 +10,38 @@ console.log(balance.toFixed(2)); //100.00
 
 const otherNumber = 23.89669
 
+
 console.log(otherNumber.toPrecision(3)); //23.9
 
-const hundreds = 100000
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString()) //10,000,000      
+
+console.log("_____________MATHS_______________")
+
+console.log(Math);
+console.log(Math.abs(-4)) //4
+console.log(Math.round(3.6)) //4
+console.log(Math.ceil(3.6)) //4
+console.log(Math.floor(3.6)) //3
+console.log(Math.sqrt(3)) //1.73
+console.log(Math.min(3,6,8,9,5,2));//2
+console.log(Math.max(3,6,8,9,5,2)); //9
+
+console.log(Math.random()); //any value between 0 to 1
+
+console.log((Math.random()*10)+1); //one digit shift +1 to avoid 0.09 type values
+console.log(Math.floor(Math.random()*10)+1); // will give one value answer
+console.log(Math.random()*100);  //two digit shift
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min );
+
+/*Math.random give us values randomly between 0 to 1 and when we multiply
+with 10 one digit is shift but in some cases we have 0.01 so it gives 0 to
+avoid this we add 1 . another problem is we have to define min and max in many 
+cases so we write (max - min + 1) max - min to match ranges and plus 1 to avoid zero
+and also we'll add min
+*/
---
+++ 02_basics/01_array.js
@@ -0,0 +1,44 @@
+//array
+
+const myArr = [0 , 1 , 2 , 3 , 4 , 5]
+console.log(myArr[1])
+
+//properties of array
+/* 
+array => object
+arrays are resizeable
+arrays are mutable
+it contain mixed datatype
+arrays are ordered
+arrays are indexed(index starts from 0)
+it make shallow copies(change in original value stack
+and heap concept)
+*/
+
+const myHeros = ["superman", "batman", "spiderman", "ironman"]
+console.log(myHeros[0]); //superman
+
+const myArr2 = new Array(1,2,3,4,5)
+console.log(myArr2[3]); //4
+
+//array methods
+
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
+
+myArr.unshift(8); //add at the beginning
+myArr.shift() //remove from the beginning
+console.log(myArr); //[0, 1, 2, 3, 4, 5, 6]
+console.log(myArr.includes(9)); //false
+console.log(myArr.indexOf(3)); // 3
+
+const newArr = myArr.join()
+console.log(newArr); //0,1,2,3,4,5,6
+console.log(myArr); //[0, 1, 2, 3, 4, 5, 6]
+
+console.log(typeof myArr); //object
+console.log(typeof newArr); //string
+
+//slice and splice
+
